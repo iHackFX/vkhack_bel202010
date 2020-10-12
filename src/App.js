bc9ad702fa614a2b6b32b7296ead4919ccdd6d2e
Alex <kacheryants30@gmail.com>
2020-10-13 01:35:54 +0300

    Button Works! Hallelujah!

--- src/App.js
+++ src/App.js
@@ -36,7 +36,7 @@ const App = () => {
 		<View activePanel={activePanel} popout={popout}>
 			<Home id='home' fetchedUser={fetchedUser} go={go} />
 			<Persik id='persik' go={go} />
-			<Advance id='Advance' go={go} />
+			<Advance id='Advance' go={go} setPopout={setPopout}/>
 		</View>
 	);
 }
--- src/panels/Advance.js
+++ src/panels/Advance.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React from "react";
 import PropTypes from "prop-types";
 import { platform, IOS, Button } from "@vkontakte/vkui";
 import Panel from "@vkontakte/vkui/dist/components/Panel/Panel";
@@ -6,14 +6,32 @@ import PanelHeader from "@vkontakte/vkui/dist/components/PanelHeader/PanelHeader
 import PanelHeaderButton from "@vkontakte/vkui/dist/components/PanelHeaderButton/PanelHeaderButton";
 import Icon28ChevronBack from "@vkontakte/icons/dist/28/chevron_back";
 import Icon24Back from "@vkontakte/icons/dist/24/back";
+import { ActionSheet, ActionSheetItem } from "@vkontakte/vkui";
 
 import Advance_img from "../img/persik.png";
 import "./Persik.css";
 
 const osName = platform();
 
+const Advance = (props) => {
+  function openBase () {
+    props.setPopout(
+      <ActionSheet onClose={() => props.setPopout(null)}>
+        <ActionSheetItem autoclose>
+          По дням
+        </ActionSheetItem>
+        <ActionSheetItem autoclose>
+          По неделям
+        </ActionSheetItem>
+        <ActionSheetItem autoclose>
+          По месяцам
+        </ActionSheetItem>
+        {osName === IOS && <ActionSheetItem autoclose mode="cancel">Отменить</ActionSheetItem>}
+      </ActionSheet>
+    );
+  }
 
-const Advance = (props) => (
+  return(
   <Panel id={props.id}>
     <PanelHeader
       left={
@@ -25,15 +43,20 @@ const Advance = (props) => (
       Advance
     </PanelHeader>
     <img className="Advance" src={Advance_img} alt="Advance The Cat" />
-    <Button size="xl" onClick={null} stretched>show me persik</Button>
-  </Panel>
-);
-
-
+    <Button
+      size="xl"
+      onClick={openBase}
+      stretched
+    >
+      show me persik
+    </Button>
+  </Panel>);
+};
 
 Advance.propTypes = {
   id: PropTypes.string.isRequired,
   go: PropTypes.func.isRequired,
+  setPropout: PropTypes.func.isRequired,
 };
 
 export default Advance;
